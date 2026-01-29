e0b5cfb368b1c463b6f920668d8f624e858165e4
Ezequote <ezequote.billing@gmail.com>
2026-01-30 01:12:15 +0500

    feat: Add 'Complex' to building type search filter

--- client/src/components/SearchInterface.tsx
+++ client/src/components/SearchInterface.tsx
@@ -905,6 +905,7 @@ export default function SearchInterface() {
                       <SelectItem value="All Types">All Types</SelectItem>
                       <SelectItem value="House">House</SelectItem>
                       <SelectItem value="Townhouse">Townhouse</SelectItem>
+                      <SelectItem value="Complex">Complex</SelectItem>
                       <SelectItem value="Apartment & Unit">Apartment & Unit</SelectItem>
                       <SelectItem value="Duplex">Duplex</SelectItem>
                       <SelectItem value="Villa">Villa</SelectItem>
